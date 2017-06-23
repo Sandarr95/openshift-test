8b24481f83fa1431aa7a1c657eb38da5b9d1541c
Sander Kolman <sanderkolman@gmail.com>
2017-06-23 12:45:47 +0200

    removed tests

--- test/openshift_test/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns openshift-test.core-test
-  (:require [clojure.test :refer :all]
-            [openshift-test.core :refer :all]))
-
-(deftest a-test
-  (testing "Succeeds always"
-    (is (= 1 1))))
