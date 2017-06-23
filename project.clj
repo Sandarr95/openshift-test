82a19e05ede5b71ddd73d61734ee44d784ef459a
Sander Kolman <sanderkolman@gmail.com>
2017-06-23 19:02:12 +0200

    Revert "added lein-ring"
    This reverts commit 2421f014dc82c2e2ebfe860f4f412db019583b3a.

--- project.clj
+++ project.clj
@@ -5,9 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [morse "0.2.4"]
-                 [aleph "0.4.3"]]
-  :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler openshift-test.core/handler :port 8080}
+                 ;[aleph "0.4.3"]
+                ]
   :main ^:skip-aot openshift-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
