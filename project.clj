1eb4f4110bf955fe1ba5c6d8bc06bc0e7388853d
Sander Kolman <sanderkolman@gmail.com>
2017-06-29 18:12:14 +0200

    do a println and depend on core.async

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.macro "0.1.5"]
-                 [aleph "0.4.3"]]
+                 [org.clojure/core.async "0.2.374"]]
   :main ^:skip-aot openshift-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/openshift_test/core.clj
+++ src/openshift_test/core.clj
@@ -1,15 +1,7 @@
 (ns openshift-test.core
-  (:require [aleph.http :as http])
   (:gen-class))
-
-(defn handler [req]
-  {:status 200
-   :headers {"content-type" "text/plain"}
-   :body "Yo, it works, gj"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (http/start-server handler {:port 8080})
-  (println "Server started")
-  (while true (Thread/sleep 100)))
+  (println "Program runs"))
