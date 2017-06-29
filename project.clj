42a2814d9a99ca72a517eab1e55b7630d888637b
Sander Kolman <sanderkolman@gmail.com>
2017-06-29 18:25:29 +0200

    put morse back

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [aleph "0.4.3"]]
+                 [aleph "0.4.3"]
+                 [morse "0.2.4"]]
   :main ^:skip-aot openshift-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
