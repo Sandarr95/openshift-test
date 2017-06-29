ee0428006a115f145d559ae35d7133ede7af2367
Sander Kolman <sanderkolman@gmail.com>
2017-06-29 18:59:17 +0200

    not requiring polling

--- src/openshift_test/core.clj
+++ src/openshift_test/core.clj
@@ -1,11 +1,9 @@
 (ns openshift-test.core
-  (:require [morse.api :as t]
-            [morse.polling :as bot])
+  (:require [morse.api :as t])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [echo-token (System/getenv "TELEGRAM_BOT_TOKEN")]
-  ;(bot/start echo-token println)
   (println "Program runs")))
