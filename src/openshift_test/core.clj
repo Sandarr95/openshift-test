a4a96c4c3879152fcb907aa41bac151f901516e0
Sander Kolman <sanderkolman@gmail.com>
2017-06-29 19:16:41 +0200

    figuring if core.async is the problem

--- src/openshift_test/core.clj
+++ src/openshift_test/core.clj
@@ -5,5 +5,6 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [echo-token (System/getenv "TELEGRAM_BOT_TOKEN")]
+  (let [some-chan (chan)
+        echo-token (System/getenv "TELEGRAM_BOT_TOKEN")]
   (println "Program runs")))
