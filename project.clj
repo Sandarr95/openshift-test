1c76ff1fcb229e6544dd8759b26712f9f59e589b
Sander Kolman <sanderkolman@gmail.com>
2017-06-29 14:25:08 +0200

    added more deps for testing

--- project.clj
+++ project.clj
@@ -4,6 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.macro "0.1.5"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [clj-stacktrace "0.2.8"]
+                 [cheshire "5.5.0"]
                  [clj-http "2.1.0"]
                  [aleph "0.4.3"]]
   :main ^:skip-aot openshift-test.core
