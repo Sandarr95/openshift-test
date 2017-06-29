b0e01faa515915ab60a0ae88fda5c6cdf4c167dd
Sander Kolman <sanderkolman@gmail.com>
2017-06-29 18:44:56 +0200

    put morse back even more

--- src/openshift_test/core.clj
+++ src/openshift_test/core.clj
@@ -1,8 +1,11 @@
 (ns openshift-test.core
-  (:require [morse.api :as t])
+  (:require [morse.api :as t]
+            [morse.polling :as bot])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Program runs"))
+  (let [echo-token (System/getenv "TELEGRAM_BOT_TOKEN")]
+  (bot/start echo-token println)
+  (println "Program runs")))
