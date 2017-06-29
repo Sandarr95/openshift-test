56255e88eaf81dde9213a55ea3fe115e1c81d397
Sander Kolman <sanderkolman@gmail.com>
2017-06-29 14:43:31 +0200

    this sounds stupid, removed some for testing

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.macro "0.1.5"]
                  [org.clojure/core.async "0.2.374"]
-                 [org.clojure/tools.logging "0.3.1"]
                  [aleph "0.4.3"]]
   :main ^:skip-aot openshift-test.core
   :target-path "target/%s"
