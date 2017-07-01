952387cd7b8c67a57624a0160c2c7bf21f20b244
Sander Kolman <sanderkolman@gmail.com>
2017-07-01 13:22:48 +0200

    im done, not rly tho :/

--- src/openshift_test/core.clj
+++ src/openshift_test/core.clj
@@ -5,6 +5,6 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [some-chan (atom 0)
+  (let [some-chan 0
         echo-token (System/getenv "TELEGRAM_BOT_TOKEN")]
   (println "Program runs")))
