fd2026fda8dc6aef0d577b136a72ac1b424f8197
Sander Kolman <sanderkolman@gmail.com>
2017-07-01 12:44:42 +0200

    figuring if atom is the problem

--- src/openshift_test/core.clj
+++ src/openshift_test/core.clj
@@ -5,6 +5,6 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [some-chan (chan)
+  (let [some-chan (atom 0)
         echo-token (System/getenv "TELEGRAM_BOT_TOKEN")]
   (println "Program runs")))
